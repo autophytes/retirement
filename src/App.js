d42f27b2cdf395ee8e2b065611e3bc290c3f5055
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2021-04-29 19:15:33 -0400

    Making some changes to see if you like to use SASS/SCSS
    Making some changes to see if you like to use SASS/SCSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './styles/index.css';
+import "./styles/index.css";
+import "./App.scss";
 
-import FinancialPlanning from './FinancialPlanning';
-import AppContextProvider from './context/appContext';
+import FinancialPlanning from "./FinancialPlanning";
+import AppContextProvider from "./context/appContext";
 
 function App() {
-	return (
-		<AppContextProvider>
-			<FinancialPlanning />
-		</AppContextProvider>
-	);
+  return (
+    <AppContextProvider>
+      <FinancialPlanning />
+    </AppContextProvider>
+  );
 }
 
 export default App;
--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,50 +1,54 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useState } from "react";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-	// STATE
-	const [profile, setProfile] = useState({
-		primary: {
-			currentAge: 29,
-			retirementAge: 65,
-			annualSavings: 10000,
-		},
-		spouse: {
-			currentAge: 29,
-			retirementAge: 65,
-			annualSavings: 10000,
-		},
-		startingInvestments: 50000,
-		retirementIncome: 70000,
-		preRetirementReturn: 0.08,
-		postRetirementReturn: 0.06,
-		inflationIncome: 0.02,
-		inflationExpenses: 0.03,
-	});
+  // STATE
+  const [profile, setProfile] = useState({
+    primary: {
+      currentAge: 29,
+      retirementAge: 65,
+      annualSavings: 10000,
+    },
+    spouse: {
+      currentAge: 29,
+      retirementAge: 65,
+      annualSavings: 10000,
+    },
+    startingInvestments: 50000,
+    retirementIncome: 70000,
+    preRetirementReturn: 0.08,
+    postRetirementReturn: 0.06,
+    inflationIncome: 0.02,
+    inflationExpenses: 0.03,
+  });
 
-	const updateProfile = useCallback((value, property, parentProperty = null) => {
-		setProfile((prev) => ({
-			...prev,
-			[parentProperty ?? property]: parentProperty
-				? {
-						...prev[parentProperty],
-						[property]: value,
-				  }
-				: value,
-		}));
-	}, []);
+  const updateProfile = useCallback(
+    (value, property, parentProperty = null) => {
+      setProfile((prev) => ({
+        ...prev,
+        [parentProperty ?? property]: parentProperty
+          ? {
+              ...prev[parentProperty],
+              [property]: value,
+            }
+          : value,
+      }));
+    },
+    []
+  );
 
-	return (
-		<AppContext.Provider
-			value={{
-				profile,
-				setProfile,
-				updateProfile,
-			}}>
-			{props.children}
-		</AppContext.Provider>
-	);
+  return (
+    <AppContext.Provider
+      value={{
+        profile,
+        setProfile,
+        updateProfile,
+      }}
+    >
+      {props.children}
+    </AppContext.Provider>
+  );
 };
 
 export default AppContextProvider;
