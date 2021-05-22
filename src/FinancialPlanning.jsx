fae4f73acd301f1fc26e7fa7f58ebfc1ffdbffb8
Josh Marbert <54632192+jmarbert@users.noreply.github.com>
2021-05-22 15:51:33 -0400

    Added page shadow, moved navbar to top tabs

--- src/FinancialPlanning.jsx
+++ src/FinancialPlanning.jsx
@@ -11,46 +11,55 @@ const App = () => {
 	const [editingName, setEditingName] = useState(false);
 
 	return (
-		<div className='page-container'>
-			{/* CONTACT NAME */}
-			{editingName ? (
-				<input
-					type='text'
-					className='client-name-title-input'
-					autoFocus
-					value={clientName}
-					size={clientName.length}
-					onFocus={(e) => e.target.select()}
-					onChange={(e) => setClientName(e.target.value)}
-					onBlur={(e) => {
-						setClientName(e.target.value || 'Sample Plan');
-						setEditingName(false);
-					}}
-				/>
-			) : (
-				<h2 className='client-name-title' onClick={() => setEditingName(true)}>
-					{clientName}
-				</h2>
-			)}
+		<>
+			{/* Shadow */}
+			<div className='page-shadow' />
 
 			{/* Module Selection Tabs */}
-			<nav className='navbar'>
-				{TABS.map((tab) => (
-					<button
-						key={tab}
-						className={'nav-tab' + (currentTab === tab ? ' active' : '')}
-						onClick={() => setCurrentTab(tab)}>
-						{tab}
-					</button>
-				))}
+			<nav className='navbar-wrapper'>
+				<nav className='navbar'>
+					{TABS.map((tab) => (
+						<div className={'nav-tab-wrapper' + (currentTab === tab ? ' active' : '')}>
+							<button
+								key={tab}
+								className={'nav-tab' + (currentTab === tab ? ' active' : '')}
+								onClick={() => setCurrentTab(tab)}>
+								{tab}
+							</button>
+						</div>
+					))}
+				</nav>
 			</nav>
 
-			{currentTab === 'Setup' && <Setup />}
-			{currentTab === 'Retirement' && <Retirement />}
-			{/* {(currentTab === 'College') && <Mockup />} */}
-			{/* {(currentTab === 'Debt') && <Mockup />} */}
-			{/* {(currentTab === 'SocialSecurity') && <Mockup />} */}
-		</div>
+			<div className='page-container'>
+				{/* CONTACT NAME */}
+				{editingName ? (
+					<input
+						type='text'
+						className='client-name-title-input'
+						autoFocus
+						value={clientName}
+						size={clientName.length}
+						onFocus={(e) => e.target.select()}
+						onChange={(e) => setClientName(e.target.value)}
+						onBlur={(e) => {
+							setClientName(e.target.value || 'Sample Plan');
+							setEditingName(false);
+						}}
+					/>
+				) : (
+					<h2 className='client-name-title' onClick={() => setEditingName(true)}>
+						{clientName}
+					</h2>
+				)}
+
+				{currentTab === 'Setup' && <Setup />}
+				{currentTab === 'Retirement' && <Retirement />}
+				{/* {(currentTab === 'College') && <Mockup />} */}
+				{/* {(currentTab === 'Debt') && <Mockup />} */}
+				{/* {(currentTab === 'SocialSecurity') && <Mockup />} */}
+			</div>
+		</>
 	);
 };
 
